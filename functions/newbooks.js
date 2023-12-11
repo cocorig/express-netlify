fce8464731dd7da8916ce4beacd4e4fd17864188
Hayeon LEE <fhdodl@naver.com>
2023-12-12 00:56:36 +0900

    fix : secrets 변수 변경

--- functions/newbooks.js
+++ functions/newbooks.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const aladinApiBaseUrl = "https://www.aladin.co.kr/ttb/api/ItemList.aspx";
 exports.handler = async (event, context) => {
   const queryType = "ItemNewAll";
-  const aladinApiUrl = `${aladinApiBaseUrl}?ttbkey=${process.env.REACT_APP_ALADIN_API_KEY}&QueryType=${queryType}&MaxResults=100&start=1&SearchTarget=Book&output=js&Cover=Big&CategoryId&Version=20131101`;
+  const aladinApiUrl = `${aladinApiBaseUrl}?ttbkey=${secrets.REACT_APP_ALADIN_API_KEY}&QueryType=${queryType}&MaxResults=100&start=1&SearchTarget=Book&output=js&Cover=Big&CategoryId&Version=20131101`;
   try {
     const response = await axios.get(aladinApiUrl);
     const data = response.data;
